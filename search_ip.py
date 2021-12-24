a7c74af51776794a529ad70c9398da71a13b6338
pandawa3 <colinpel23@outlook.fr>
2021-12-24 11:38:24 +0100

    python3 compatibility update

--- search_ip.py
+++ search_ip.py
@@ -6,28 +6,26 @@ SHODAN_API_KEY = "SHODAN_API_KEY"
 api = shodan.Shodan(SHODAN_API_KEY)
 PAGE = sys.argv[1]
 PAGE2 = int(PAGE) + 1
-try:
-        # Search Shodan
-        results = api.search('port:111 nfs country:fr', page=PAGE)
-
-        # Show the results
-        print 'Results found: %s' % results['total']
-                
-        for result in results['matches']:
-                print result['ip_str'].rstrip()
- 
-except shodan.APIError, e:
-        print 'Error: %s' % e
 
 try:
-        # Search Shodan
-        results = api.search('port:111 nfs country:fr', page=PAGE2)
+    # Search Shodan
+    results = api.search('port:445 country:fr disabled', page=PAGE)
 
-      
-       
-                
-        for result in results['matches']:
-                print result['ip_str'].rstrip() 
- 
-except shodan.APIError, e:
-        print 'Error: %s' % e
+    # Show the results
+    print('Results found: %s' % results['total'])
+
+    for result in results['matches']:
+        print(result['ip_str'].rstrip())
+
+except shodan.APIError as e:
+    print('Error: %s' % e)
+
+try:
+    # Search Shodan
+    results = api.search('port:445 country:fr disabled', page=PAGE2)
+
+    for result in results['matches']:
+        print(result['ip_str'].rstrip())
+
+except shodan.APIError as e:
+    print('Error: %s' % e)
--- search_url.py
+++ search_url.py
@@ -6,36 +6,32 @@ SHODAN_API_KEY = "SHODAN_API_KEY"
 api = shodan.Shodan(SHODAN_API_KEY)
 PAGE = sys.argv[1]
 PAGE2 = int(PAGE) + 1
-try:
-        # Search Shodan
-        results = api.search('http.title:"Node-RED"', page=PAGE)
-
-        # Show the results
-        print 'Results found: %s' % results['total']
-       
-        for result in results['matches']:
-		try:
-			if result['ssl']:
-                		print "https://" + result['ip_str'].rstrip() + ":" + str(result['port'])
-			
-		except:
-			print "http://" + result['ip_str'].rstrip() + ":" + str(result['port'])
-except shodan.APIError, e:
-        print 'Error: %s' % e
 
 try:
-        # Search Shodan
-        results = api.search('http.title:"Node-RED"', page=PAGE2)
+    # Search Shodan
+    results = api.search('http.title:"Node-RED"', page=PAGE)
 
-      
-       
-                
-        for result in results['matches']:
-		try:
-			if result['ssl']:
-                		print "https://" + result['ip_str'].rstrip() + ":" + str(result['port'])
-			
-		except:
-			print "http://" + result['ip_str'].rstrip() + ":" + str(result['port'])
-except shodan.APIError, e:
-        print 'Error: %s' % e
+    # Show the results
+    print('Results found: %s' % results['total'])
+
+    for result in results['matches']:
+        try:
+            if result['ssl']:
+                print("https://" + result['ip_str'].rstrip() + ":" + str(result['port']))
+        except:
+            print("http://" + result['ip_str'].rstrip() + ":" + str(result['port']))
+except shodan.APIError as e:
+    print('Error: %s' % e)
+
+try:
+    # Search Shodan
+    results = api.search('http.title:"Node-RED"', page=PAGE2)
+
+    for result in results['matches']:
+        try:
+            if result['ssl']:
+                print("https://" + result['ip_str'].rstrip() + ":" + str(result['port']))
+        except:
+            print("http://" + result['ip_str'].rstrip() + ":" + str(result['port']))
+except shodan.APIError as e: \
+        print('Error: %s' % e)
